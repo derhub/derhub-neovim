40cceb622f99139a4618a4f285af312dbdcc7efc
Johnder <imjohnderbaul@gmail.com>
2025-07-22 00:06:23 +0800

    add lualine

--- lua/derhub/plugins/treesitter.lua
+++ lua/derhub/plugins/treesitter.lua
@@ -1,12 +1,13 @@
 return {
   "nvim-treesitter/nvim-treesitter", 
   build = ":TSUpdate",
+  main = 'nvim-treesitter.configs',
   config = function()
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "lua", "vim", "vimdoc", "javascript", "typescript", "php", "html" },
-          sync_install = false,
+          ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'typescript', 'php' },
+	  auto_install = true,
           highlight = { enable = true },
           indent = { enable = true },
         })
